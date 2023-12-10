50c1d5f38f58c1641d8e46cc4a05e3a2dc183f85
maheen-naeem <150660844+maheen-naeem@users.noreply.github.com>
2023-12-10 13:56:40 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// print hello world
+console.log("Hello World");
+// print hello my name
+console.log("Hello Maheen");
---
+++ main.ts
@@ -0,0 +1,7 @@
+// print hello world
+
+console.log("Hello World");
+
+// print hello my name
+
+console.log("Hello Maheen");
